641dfeef0be29f5c0ea4707deaf77dc0bf13c0d7
Thomas Jouas <thomas.jouas@letgo.com>
2017-03-19 10:19:00 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"github.com/gorilla/websocket"
+	"github.com/olorin/nagiosplugin"
+	"log"
+	"net/url"
+)
+
+var addr = flag.String("addr", "echo.websocket.org", "http service address")
+
+func main() {
+	flag.Parse()
+
+	u := url.URL{Scheme: "wss", Host: *addr, Path: "/"}
+
+	// Initialize the check - this will return an UNKNOWN result
+	// until more results are added.
+	check := nagiosplugin.NewCheck()
+	// If we exit early or panic() we'll still output a result.
+	defer check.Finish()
+
+	// Connect to the server
+	log.Printf("Connecting to %s", u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		check.Criticalf("Cannot connect to websocket")
+		log.Fatal("dial:", err)
+	} else {
+		log.Println("connected")
+	}
+	defer c.Close()
+
+	// close the connection
+	log.Println("Requesting connection closure")
+	err = c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		log.Println("write close:", err)
+		check.Criticalf("Error while closing websocket")
+		return
+	} else {
+		check.AddResult(nagiosplugin.OK, "Websocket server is working correctly")
+	}
+
+}
